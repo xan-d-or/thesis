b07a29bd826e1cc7a7b9b1c7fe1d23928cbbe02f
Aleksandr Makarenko <alexrommak@gmail.com>
2025-02-14 00:51:11 +0300

    Оцнил фиксированные эффекты для HS0

--- data/scripts/fixed_effects_computation.R
+++ data/scripts/fixed_effects_computation.R
@@ -3,60 +3,11 @@
 library(tidyverse)
 library(arrow)
 library(fixest)
-setwd("C:/Users/unstr/Documents/7 семестр работы/Диплом/thesis-repo/thesis")
-getwd()
 
-# Оборачиваю подсчёт фиксированных эффектов в функцию
-compute_fe = function(dataDirectory, 
-                      isCategoryAggregated, 
-                      valueOrQuantity = 'value', 
-                      plainOrChange = 'plain'){
-  
-  if(dataDirectory == "data/preprocessed_data/trade_hs0.parquet.gzip" & isCategoryAggregated){
-    stop('В файле нет категории продукта!')
-  }
-  
-  df = read_parquet(dataDirectory)
-  
-  # Рассчитываем приросты
-  if(plainOrChange == 'change'){
-    if (valueOrQuantity == "value"){
-      df[,'log_change'] = log(df$v) - log(df$v19)
-    } else {
-      df[,'log_change'] = log(df$q) - log(df$q19)
-    } 
-  }
-  # Прописываем зависимую переменную и формируем формулу
-  if(plainOrChange == 'change'){
-    y = 'log_change'
-  } else{
-    if (valueOrQuantity == "value"){
-      y = 'log(v)'
-    } else {
-      y = 'log(q)'
-    }
-  }
-  fml = formula(paste(y, '~1| export_fe+import_fe+bilateral_fe'))
-  # Считаем переменные, по которым потом считаются фиксированные эффекты
-  if (isCategoryAggregated){
-    df = df %>% mutate(export_fe = paste(t, "_", i, "_", category),
-                       import_fe = paste(t, "_", j, "_", category),
-                       bilateral_fe = paste(i, "_", j, "_", category))
-  } else {
-    df = df %>% mutate(export_fe = paste(t, "_", i),
-                       import_fe = paste(t, "_", j),
-                       bilateral_fe = paste(i, "_", j))
-   
-  }
-  print('Считаем фиксированные эффекты')
-  m = feols(fml, data = df)
-  list(model = m, fe = m %>% fixef())
-}
-
-dataframe_from_named_list = function(my_list, isCategoryAggregated){
+DataframeFromNamedList = function(my_list, is_category_aggregated){
   # Convert named list to dataframe
   name_matrix <- do.call(rbind, strsplit(names(my_list), " _ "))
-  if (isCategoryAggregated){
+  if (is_category_aggregated){
     result_df <- data.frame(
       year = name_matrix[, 1],
       country = name_matrix[, 2],
@@ -73,14 +24,65 @@ dataframe_from_named_list = function(my_list, isCategoryAggregated){
     )
   }
   result_df$year <- as.integer(result_df$year)
-  result_df
+  return(result_df)
 }
 
+# Оборачиваю подсчёт фиксированных эффектов в функцию
+ComputeFE = function(data_directory, 
+                      is_category_aggregated, 
+                      value_or_quantity = 'value', 
+                      plain_or_change = 'plain',
+                     save_path,
+                     fixef.tol = 1e-5){
+  
+  if(data_directory == "data/preprocessed_data/trade_hs0.parquet.gzip" & is_category_aggregated){
+    stop('В файле нет категории продукта!')
+  }
+  
+  
+  df = read_parquet(data_directory)
+  
+  # Рассчитываем приросты
+  if(plain_or_change == 'change'){
+    if (value_or_quantity == "value"){
+      df[,'log_change'] = log(df$v) - log(df$v19)
+    } else {
+      df[,'log_change'] = log(df$q) - log(df$q19)
+    } 
+  }
+  # Прописываем зависимую переменную и формируем формулу
+  if(plain_or_change == 'change'){
+    y = 'log_change'
+  } else{
+    if (value_or_quantity == "value"){
+      y = 'log(v)'
+    } else {
+      y = 'log(q)'
+    }
+  }
+  fml = formula(paste(y, '~1| export_fe+import_fe+bilateral_fe'))
+  # Считаем переменные, по которым потом считаются фиксированные эффекты
+  if (is_category_aggregated){
+    df = df %>% mutate(export_fe = paste(t, "_", i, "_", category),
+                       import_fe = paste(t, "_", j, "_", category),
+                       bilateral_fe = paste(i, "_", j))
+  } else {
+    df = df %>% mutate(export_fe = paste(t, "_", i),
+                       import_fe = paste(t, "_", j),
+                       bilateral_fe = paste(i, "_", j))
+   
+  }
 
-res = compute_fe("data/preprocessed_data/trade_hs0.parquet.gzip", 
-                 isCategoryAggregated = F, 
-                 valueOrQuantity = "value",
-                 plainOrChange = 'plain') 
-res$model
-res$fe$export_fe %>% dataframe_from_named_list(., F) %>% head()
+  m = feols(fml, data = df, fixef.tol = fixef.tol)
+  print(m)
+  
+  fixed_effects = m %>% fixef()
+  
+  fixed_effects$export_fe %>% DataframeFromNamedList(., is_category_aggregated) %>%
+    write_parquet(., paste(save_path, '_',  value_or_quantity, '_', plain_or_change, '_export_fe.parquet.gzip', sep = ''))
+  fixed_effects$import_fe %>% DataframeFromNamedList(., is_category_aggregated) %>%
+    write_parquet(., paste(save_path, '_',  value_or_quantity, '_', plain_or_change, '_import_fe.parquet.gzip', sep = ''))
+  # return(list(model = m, fe = m %>% fixef()))
+}
+
 
